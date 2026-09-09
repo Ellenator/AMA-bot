719936c031406a8b0aeff82a1b6068795fb71dc0
Ellenator <eaa23emsm@students.eaaa.dk>
2026-09-09 13:36:07 +0200

    opretelse af array

--- server.js
+++ server.js
@@ -4,11 +4,21 @@ const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
-
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+const messages = [];
 
 app.get("/", (req, res) => {
-    res.render("index");
+    res.render("index", { messages });
+});
+
+app.post("/ask", (req, res) => {
+    const question = req.body.question;
+
+    messages.push(question);
+
+    res.render("index", { messages });
 });
 
 app.listen(port, () => {
