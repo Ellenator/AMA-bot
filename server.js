35da53b39b32bda2d382e6a35bf26b52f144c3dd
Ellenator <eaa23emsm@students.eaaa.dk>
2026-09-09 15:01:45 +0200

    answers array

--- server.js
+++ server.js
@@ -9,15 +9,46 @@ app.use(express.urlencoded({ extended: true }));
 
 const messages = [];
 
+const answers = [
+  {
+    keywords: ["navn", "hedder", "hvem er du"],
+    answer: "Jeg hedder Ellen, men er også kendt som Ellenator i sjove sammenhænge. Hvilke facts vil du ellers vide om mig?"
+  },
+  {
+    keywords: ["bor", "by", "fra"],
+    answer: "Jeg bor inde i Aarhus by sammen med min kæreste Nick."
+  },
+  {
+    keywords: ["fritid", "hobby", "kan lide"],
+    answer: "I min fritid kan jeg godt lide at styrketræne, løbe, lave mad, bage lækker kage og brød og meget andet"
+  }
+];
+
+function findAnswer(question) {
+  const normalizedQuestion = question.toLowerCase();
+
+  for (const answerGroup of answers) {
+    const hasMatch = answerGroup.keywords.some((keyword) => normalizedQuestion.includes(keyword));
+
+    if (hasMatch) {
+      return answerGroup.answer;
+    }
+  }
+
+  return "Det kender jeg desværre ikke svaret på endnu.";
+}
+
 app.get("/", (req, res) => {
     res.render("index", { messages });
 });
 
 app.post("/ask", (req, res) => {
     const question = req.body.question;
-
+    
     messages.push({ type: "question", text: question });
-    messages.push({ type: "answer", text: "Jeg leder efter et svar..." })
+
+    const answer = findAnswer(question);
+    messages.push({ type: "answer", text: answer });
 
     res.render("index", { messages });
 });
