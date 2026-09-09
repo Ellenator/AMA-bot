4fe150c0aaae64dd4c7bd394379968e42b0ade3c
Ellenator <eaa23emsm@students.eaaa.dk>
2026-09-09 12:43:04 +0200

    opdatering af stier og rendering med ejs

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const port = 3000;
 
 app.use(express.static("public"));
 
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
