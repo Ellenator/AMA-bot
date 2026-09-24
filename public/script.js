6055ae3a203af02e973debd7b23107e1538ceced
Ellenator <eaa23emsm@students.eaaa.dk>
2026-09-24 17:57:23 +0200

    Save AMAbot before splitting into client and server

---
+++ public/script.js
@@ -0,0 +1,26 @@
+
+/*
+//keyboard events
+document.addEventListener('keydown', 
+  function(event) {
+    console.log('Tast trykket;', event.key)
+    if (event.key === 'Escape') {
+      console.log('Escape blev trykket!')
+    }
+  })
+
+
+//form events
+let input = document.querySelector('input')
+input.addEventListener('input', function(event) {
+    console.log('Value:', event.target.value)
+})
+let form = document.querySelector('form')
+form.addEventListener('submit', function(event) {
+    event.preventDefault() //stop form submission
+    console.log('Form blev submittet')
+    console.log('Form data:', new FormData(form))
+})
+*/
+
+//max charactercount in form
