b787c800fa03ab86153c937858c451f3da48df90
Ellenator <eaa23emsm@students.eaaa.dk>
2026-09-24 18:31:04 +0200

    fjernelse af ejs

--- server/server.js
+++ server/server.js
@@ -5,9 +5,7 @@ import fs from "node:fs/promises";
 const app = express();
 const port = 3000;
 
-app.use(express.static("public"));
-app.set("view engine", "ejs");
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 async function loadMessages() {
   const data = await fs.readFile("./data/messages.json", "utf8");
