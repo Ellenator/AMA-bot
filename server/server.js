9d3360769f5d5b72cc849a8dbdfb386d13b8faa7
Ellenator <eaa23emsm@students.eaaa.dk>
2026-09-25 15:26:56 +0200

    answers filomstrukturering der virker yippiiee! og self. m. CRUD ;)

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { readFile } from "node:fs";
-import fs from "node:fs/promises";
+//import { readFile } from "node:fs";
+//import fs from "node:fs/promises";
 import messagesRouter from "../server/routes/messages.js";
+import answersRouter from "../server/routes/answers.js";
 
 const app = express();
 const port = 3000;
@@ -9,17 +10,8 @@ const port = 3000;
 app.use(express.json());
 
 app.use("/messages", messagesRouter);
+app.use("/answers", answersRouter);
 
-//AMA bot svar
-async function loadAnswers() {
-  const data = await fs.readFile("./data/answers.json", "utf8");
-  return JSON.parse(data);
-}
-
-async function saveAnswers(answers) {
-  const json = JSON.stringify(answers, null, 2);
-  await fs.writeFile("./data/answers.json", json);
-}
 
 function countMatches(keywords, normalizedQuestion) {
     const matches = keywords.filter((keyword) => 
@@ -70,54 +62,6 @@ const topicStats = {
     hobbier: 0,
     frygter: 0
 };
-
-//routes answers
-app.get("/answers", async (req, res) => {
-  const answers = await loadAnswers();
-
-  res.json(answers);
-});
-
-app.get("/answers/:category", async (req, res) => {
-  const answers = await loadAnswers();
-  const answerRule = answers.find((a) => a.category === req.params.category);
-
-  res.json(answerRule);
-});
-
-app.post("/answers", async (req, res) => {
-  const answers = await loadAnswers();
-  const newAnswerRule = {
-    category: req.body.category,
-    keywords: req.body.keywords,
-    answer: req.body.answer
-  };
-
-  answers.push(newAnswerRule);
-  await saveAnswers(answers);
-
-  res.json(newAnswerRule);
-});
-
-app.put("/answers/:category", async (req, res) => {
-  const answers = await loadAnswers();
-  const answerRule = answers.find((a) => a.category === req.params.category);
-
-  answerRule.keywords = req.body.keywords;
-  answerRule.answer = req.body.answer;
-  await saveAnswers(answers);
-
-  res.json(answerRule);
-});
-
-app.delete("/answers/:category", async (req, res) => {
-  const answers = await loadAnswers();
-  const updatedAnswers = answers.filter((a) => a.category !== req.params.category);
-
-  await saveAnswers(updatedAnswers);
-
-  res.send();
-});
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
